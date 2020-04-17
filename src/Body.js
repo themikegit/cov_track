8a32e32ef5ac5173b22eb83a85bce683cd3fe542
themikegit <rostovniki5@gmail.com>
2020-04-17 19:26:45 +0200

    render all results

--- src/Body.js
+++ src/Body.js
@@ -4,21 +4,34 @@ import {Context} from './Context'
 export default function Body() {
 const [global, countries,searchers] = useContext(Context)
 
+const searchResult  = searchers.map( item => <Data
+    key = {item.Slug}
+    code = {item.CountryCode}
+    country = {item.Country}
+    newconfirmed = {item.NewConfirmed}
+    newrecovered = {item.NewRecovered}
+    newdeaths = {item.NewDeaths}
+    totaldeaths = {item.TotalDeaths}
+    totalrecovered = {item.TotalRecovered}
+    totalconfirmed = {item.TotalConfirmed}
+    date = {item.Date}
+    /> )
 
+    const allcount  = countries.map( item => <Data
+        key = {item.Slug}
+        code = {item.CountryCode}
+        country = {item.Country}
+        newconfirmed = {item.NewConfirmed}
+        newrecovered = {item.NewRecovered}
+        newdeaths = {item.NewDeaths}
+        totaldeaths = {item.TotalDeaths}
+        totalrecovered = {item.TotalRecovered}
+        totalconfirmed = {item.TotalConfirmed}
+        date = {item.Date}
+        /> )
     return (
         <div id="contain" >
-            {searchers.map( item => <Data
-            key = {item.Slug}
-            code = {item.CountryCode}
-            country = {item.Country}
-            newconfirmed = {item.NewConfirmed}
-            newrecovered = {item.NewRecovered}
-            newdeaths = {item.NewDeaths}
-            totaldeaths = {item.TotalDeaths}
-            totalrecovered = {item.TotalRecovered}
-            totalconfirmed = {item.TotalConfirmed}
-            date = {item.Date}
-            /> )}
+            { searchResult.length > 0 ? searchResult : allcount  }
         </div>
     )
 }
